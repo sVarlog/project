e5271a5d28380b000b824c1f19a33cc7c6195a9c
sVarlog <giroild@gmail.com>
2020-10-08 23:07:56 +0300

    lesson 15

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 'use strict';
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-let lastFilm = prompt('Один из последних посмотренных фильмов');
-let review = prompt('На сколько оцените его?');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,6 +8,23 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
-personalMovieDB.movies[lastFilm] = review;
 
-console.log(personalMovieDB);
+for (let i = 0; i < numberOfFilms;){
+    let q = prompt("Один из последних посмотренных фильмов", "");
+    let a = prompt("На сколько оцените его?", "");
+    if(q != '' && q != null && q.length < 50){
+        if(a != '' && a != null && a.length < 50){
+            i++;
+        }
+    }
+    personalMovieDB.movies[q] = a;
+}
+if(personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель')
+} else if(personalMovieDB.count > 30){
+    alert('Вы киноман');
+} else{
+    alert('error');
+}
